41517eaeb8ee9e4aa21f24ae8a993e7040d89c24
lucasdj25 <lucasjmax@gmail.com>
2022-10-02 16:13:36 -0400

    File transfers (does not correct packet loss yet)

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 
+from concurrent.futures import process
 import pickle
 import socket
 import packet
@@ -6,54 +7,61 @@ import packet
 SERVER_ADDR = ("127.0.0.1", 8080)
 RECIEVE_WINDOW_SIZE = 5
 
+def processPacket(pkt, file):
+    file.write(pkt.data)
+
 def receive(sock, filename):
 
     bufferedAcks = []
-    LARGEST_ACCEPTABLE_FRAME = RECIEVE_WINDOW_SIZE
-    LAST_FRAME_RECIEVED = -1
     SeqNumToAck = 0
+    LARGEST_ACCEPTABLE_FRAME = RECIEVE_WINDOW_SIZE - 1
+    LAST_FRAME_RECIEVED = -1
 
-    """ try:
+    try:
         file = open(filename, 'wb')
     except IOError:
-        print("Can't open %s" % filename) """
+        print("Can't open %s" % filename)
     
     while True:
         pkt, address = sock.recvfrom(1099)
 
         packt: packet = pickle.loads(pkt)
         
-        # If packet seq_num is outside of window range, it is ignored
-        if(LAST_FRAME_RECIEVED < packt.seq_num <= LARGEST_ACCEPTABLE_FRAME):
-
-            # if the packet is the next expected, send acknowledgement and process packet
-            if(packt.seq_num == SeqNumToAck):
-                # if the packet is shorter then expected, it's the last packet
-                if(len(packt.data) < 1024):
+        # If packet seq_num is below of window range, ack was lost and should be resent
+        if(LAST_FRAME_RECIEVED < packt.seq_num):
+            if(packt.seq_num <= LARGEST_ACCEPTABLE_FRAME):
+                # if the packet is the next expected, send acknowledgement and process packet
+                if(packt.seq_num == SeqNumToAck):
+                    # if the packet is shorter then expected, it's the last packet
+                    if(len(packt.data) < 1024):
+                        print("Sending Ack for packet: %d" % packt.seq_num)
+                        processPacket(packt, file)
+                        print("COMPLETE")
+                        send_ack(packt.seq_num, sock, address)
+                        break
+                    processPacket(packt, file)
                     print("Sending Ack for packet: %d" % packt.seq_num)
-                    print("COMPLETE")
                     send_ack(packt.seq_num, sock, address)
-                    break
-                print("Sending Ack for packet: %d" % packt.seq_num)
-                send_ack(packt.seq_num, sock, address)
 
-                # TODO - Process Packet (write file)
-
-                
-                # set next expected sequence number and moves window accordingly
-                if(SeqNumToAck + 1 > 9):
-                    LAST_FRAME_RECIEVED = -1
-                    LARGEST_ACCEPTABLE_FRAME = RECIEVE_WINDOW_SIZE
-                    SeqNumToAck = 0
-                else:
+                    # set next expected sequence number and moves window accordingly
                     LAST_FRAME_RECIEVED = SeqNumToAck
                     LARGEST_ACCEPTABLE_FRAME = LAST_FRAME_RECIEVED + RECIEVE_WINDOW_SIZE
                     SeqNumToAck += 1
-            else:
-                # if the packet is not the next packet expected but is within the window, put into buffer
-                bufferedAcks.push(packt)
 
-                # TODO - find out when to process buffer
+                    for packt in bufferedAcks:
+                        if packt.seq_num == SeqNumToAck:
+                            processPacket(packt, file)
+                            send_ack(packt.seq_num, sock, address)
+                            LAST_FRAME_RECIEVED = SeqNumToAck
+                            LARGEST_ACCEPTABLE_FRAME = LAST_FRAME_RECIEVED + RECIEVE_WINDOW_SIZE
+                            SeqNumToAck += 1
+                else:
+                    # if the packet is not the next packet expected but is within the window, put into buffer
+                    bufferedAcks.append(packt)
+                    print("Buffered Packet: %d" % packt.seq_num)
+
+        else:
+            send_ack(packt.seq_num, sock, address)
 
 
 
@@ -65,11 +73,11 @@ if __name__ == "__main__":
     # TODO - uncomment later, remove hardcode address
 
     # Asks for IP Address, Port number, and filename
-    # ipAddress = input("Enter an IP Address: ")
+    ipAddress = input("Enter an IP Address: ")
 
-    # port = input("Enter a port number: ")
+    port = input("Enter a port number: ")
 
-    # SERVER_ADDR = (ipAddress, port)
+    SERVER_ADDR = (ipAddress, int(port))
 
     filename = input("Enter a FileName: ")
 
--- server.py
+++ server.py
@@ -1,34 +1,41 @@
-from curses import window
+
 import socket
-from sys import getsizeof
 import packet
 import pickle
 
 SENDER_WINDOW_SIZE = 5
-LAST_ACKNOWLEDGEMENT_RECIEVED = -1
-LAST_FRAME_SENT = 0
 MAX_SEQ_NUM = SENDER_WINDOW_SIZE * 2
 CLIENT_ADDR = ("127.0.0.1", 8080)
 
-def get_packets(file_name):
+def get_packet(data, seqNum):
 
-    packets = []
-    seqNum = -1
-
-    with open(file_name, 'rb') as data:
-        while True:
-            buf = data.read(1024) 
-            if not buf:
-                break
-            if(seqNum >= MAX_SEQ_NUM - 1):
-                seqNum = 0
-            else:
-                seqNum += 1
-            packt = packet.packet(seqNum, b'data', buf)
-            packets.append(packt)
-
-        return packets   
+    buf = data.read(1024) 
+    if buf:
+        packt = packet.packet(seqNum, b'data', buf)
+        return packt 
+    else:
+        return "END" 
     
+
+def ack_recieved(ackNum, sendWindow, fp, sock, addr):
+    global LAST_FRAME_SENT
+    for p in sendWindow:
+        if p.seq_num == ackNum:
+            LAST_FRAME_SENT += 1
+            packt = get_packet(fp, LAST_FRAME_SENT)
+            if(packt != "END"):
+                p = packt
+                pickle_packet = pickle.dumps(packt, pickle.HIGHEST_PROTOCOL)
+                sock.sendto(pickle_packet, addr)
+                print("Sending Packet: %d" % p.seq_num)
+            else:
+                return "BREAK"
+        if p.seq_num < ackNum:
+            pickle_packet = pickle.dumps(p, pickle.HIGHEST_PROTOCOL)
+            sock.sendto(pickle_packet, addr)
+    return sendWindow
+ 
+
 
 if __name__ == "__main__":
 
@@ -45,19 +52,34 @@ if __name__ == "__main__":
     # decode the name
     decoded_name = file_name.decode()
 
-    # get array of packet objects
-    packets_to_send = get_packets(decoded_name)
+    fp = open(decoded_name, 'rb')
 
+    sendWindow = []
+    global LAST_FRAME_SENT
+    LAST_FRAME_SENT = -1
 
+    # Reads first 5 packets
+    for i in range(SENDER_WINDOW_SIZE):
+        packt = get_packet(fp, i)
+        if(packt != "END"):
+            sendWindow.append(packt)
 
-    for p in packets_to_send:
-        print("Packet.data size before pickle: %d" % len(p.data))
+    # Sends packets current in the window
+    for p in sendWindow:
+        print("Sending Packet: %d" % p.seq_num)
         pickle_packet = pickle.dumps(p, pickle.HIGHEST_PROTOCOL)
-        print("len(packet) = %d" % len(pickle_packet))
-        
         sock.sendto(pickle_packet, addr)
-        print("sent packet %d to client" % p.seq_num)
+        LAST_FRAME_SENT += 1
+    
+    # waits for an ack
+    while True:
+        print("AWAITING ACKNOWLEDGEMENTS")
+        ack, addr = sock.recvfrom(1024)
+        ackNum = int.from_bytes(ack, byteorder='little')
+        print("Ack received:", ackNum)
+        sendWindow = ack_recieved(ackNum, sendWindow, fp, sock, addr) 
+        if(sendWindow == "BREAK"):
+            break
 
-    # while True:
-    #     ack, addr = sock.recvfrom(1024)
-    #     print("ack received:", int.from_bytes(ack, byteorder='little'))
+    print("FILE TRANSFERRED")
+
